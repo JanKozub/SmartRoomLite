17b6239d922241a925c3c07ae79fcdfc678dd507
JanKozub <j.kozub136@gmail.com>
2019-11-11 01:43:01 +0100

    -Fixes to overall layout and sliders styling

--- frontend/smlite/src/components/slider/slider.component.ts
+++ frontend/smlite/src/components/slider/slider.component.ts
@@ -6,22 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./slider.component.sass']
 })
 export class SliderComponent {
-  autoTicks = true;
-  disabled = false;
-  invert = false;
-  max = 5;
-  min = 0;
-  showTicks = false;
-  step = 1;
-  thumbLabel = false;
-  value = 2;
-  vertical = true;
-
-  get tickInterval(): number | 'auto' {
-    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
-  }
-  set tickInterval(value) {
-
-  }
-  private _tickInterval = 1;
+  value = 3;
 }
